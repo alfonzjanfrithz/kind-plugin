8fbf842f3a0c3c8109f056a2237df302e158ed26
GitHub Action <action@github.com>
2020-12-17 14:51:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alfonzjanfrithz/kindplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alfonzjanfrithz.kindplugin.services
+
+import com.github.alfonzjanfrithz.kindplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
